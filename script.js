bf5039fac84fe02dc41f218aabe29d2c6a01d6b0
murilosoarez <murilosoliveira18@gmail.com>
2023-11-05 15:19:59 -0300

    [Fix Visor display]

--- script.js
+++ script.js
@@ -2,89 +2,113 @@ const buttons = document.querySelectorAll('button')
 const body = document.body 
 
 let Visor = document.getElementsByClassName('Visor')
+let operationDisplay = document.createElement('div')
+operationDisplay.className = 'Operation Display'
+let numberDisplay = document.createElement('div')
+numberDisplay.className = 'Number Display'
+
+Visor[0].append(operationDisplay)
+Visor[0].append(numberDisplay)
+
 let number = ''
 
 var n1 = null
 var n2 = null
 var operator = undefined
 
-function calculator(a, b) {
-
-    while(Visor[0].hasChildNodes()) {
-        Visor[0].removeChild(Visor[0].firstChild)
-    }   
-
-    if (operator === 'sum') {
-        op = a + b
-        Visor[0].append(op)
-    }
-
-    else if (operator === 'subtract') {
-        op = a - b
-        Visor[0].append(op)
-    }
-
-    else if (operator === 'multiply') {
-        op = a * b
-        Visor[0].append(op)
-    }
-    else if (operator === 'division') {
-        op = a / b
-        Visor[0].append(op)
-    }
-
-    operator = undefined
-    number = ''
-    n1 = op 
-}
+var displayController = false
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
 
         if (button.className === 'number') {
-            Visor[0].append(button.innerHTML)
+
+            if (displayController === true) {
+                while (numberDisplay.hasChildNodes()) {
+                    numberDisplay.removeChild(numberDisplay.firstChild)
+                }
+                displayController = false
+            }
+
+            numberDisplay.append(button.innerHTML)
             number += button.innerHTML
+            
+            if (n1 !== null) {
+                n2 = parseInt(number)
+            }
         }
 
         if (button.className === 'operator') {
             
-            while(Visor[0].hasChildNodes()) {
-                Visor[0].removeChild(Visor[0].firstChild)
-            }   
+            displayController = true 
+
+            operationDisplay.append(number)
             
-            // This section has the purpose of defining which operating will be made according to the click
-
+            if (n1 === null) {
+                n1 = parseInt(number)
+            }
+    
             if (operator === undefined) {
-                if (n1 === null) { // Fills n1 value if it's the first time the value is being entered
-                    n1 = parseInt(number)
-                }
 
-                
                 if (button.id === 'sum') {
+                    operationDisplay.append('+')
                     operator = 'sum' 
                 }
 
                 else if (button.id === 'subtract') {
+                    operationDisplay.append('-')
                     operator = 'subtract'
                 }
                 
                 else if (button.id === 'division') {
+                    operationDisplay.append('/')
                     operator = 'division'
                 }
                 
                 else if (button.id === 'multiply') {
+                    operationDisplay.append('*')
                     operator = 'multiply'
                 }
 
-                number = '' // String variable 'number' is renitialized
-
+                number = '' // String variable 'number' is renitialized 
             }
             
-            if (button.id === 'equal' && n1 !== null) { // This section is responsible to determine if n1 is already filled so a calculus between two numbers can be made.
-                n2 = parseInt(number)
+            if (n1 !== null && n2 !== null)  {
                 calculator(n1, n2)
-            }
+            }     
         }
     })
 })
+
+function calculator(a, b) {
+
+    while (numberDisplay.hasChildNodes()) {
+        numberDisplay.removeChild(numberDisplay.firstChild)
+    }
+
+    if (operator === 'sum') {
+        op = a + b
+    }
+
+    else if (operator === 'subtract') {
+        operationDisplay.append('-')
+        op = a - b
+    }
+
+    else if (operator === 'multiply') {
+        operationDisplay.append('*')
+        op = a * b
+    }
+
+    else if (operator === 'division') {
+        operationDisplay.append('/')
+        op = a / b
+    }
+
+    numberDisplay.append(op)
+
+    operator = undefined
+    number = ''
+    n1 = op 
+}
 
