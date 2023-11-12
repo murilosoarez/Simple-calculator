cc3cc2d222f8b272a41f056cf0b77d0dac609c1e
murilosoarez <murilosoliveira18@gmail.com>
2023-11-12 13:31:18 -0300

    [Otimized code with object structure and functions]

--- script.js
+++ script.js
@@ -3,128 +3,85 @@ const operationDisplay = document.getElementsByClassName('Operation Display')
 const numberDisplay = document.getElementsByClassName('Number Display')
 
 let buttonStringContent = ''
-let VAL1 = null 
-let VAL2 = null
-let operator = undefined
-let operationResult
-let equalButton = false 
-let numberDisplayController = false 
 
-function deleteOperationDisplayElements() {
-    while (operationDisplay[0].hasChildNodes()) {
-        operationDisplay[0].removeChild(operationDisplay[0].firstChild)
-    }
+const operation = {
+    sum: '+',
+    subtract: '-',
+    multiply: '*',
+    division: '/'
 }
 
-function deleteNumberDisplayElements() {
+let typeOfOperation
+
+function operationChooser(btnID) {
+    Object.keys(operation).forEach(symbol => {
+        if (btnID === operation[symbol]) {
+            operationDisplay[0].append(operation[symbol])
+            typeOfOperation = operation[symbol]
+        }
+        buttonStringContent = ''
+    })
+}
+
+function deleteNumberDisplay() {
     while (numberDisplay[0].hasChildNodes()) {
         numberDisplay[0].removeChild(numberDisplay[0].firstChild)
     }
 }
 
-function addArithmeticSymbol(BTN_ID) {
-
-    if (BTN_ID === 'sum') {
-        operationDisplay[0].append('+')
-        operator = 'sum'
-    }
-    if (BTN_ID === 'subtract') {
-        operationDisplay[0].append('-')
-        operator = 'subtract'
-    }
-    if (BTN_ID === 'multiply') {
-        operationDisplay[0].append('*')
-        operator = 'multiply'
-    }
-    if (BTN_ID === 'divide') {
-        operationDisplay[0].append('/')
-        operator = 'divide'
+function deleteOperationDisplay() {
+    while (operationDisplay[0].hasChildNodes()) {
+        operationDisplay[0].removeChild(operationDisplay[0].firstChild)
     }
 }
 
+let VAL1 = null
+let VAL2 = null
+let numberDisplayController = false 
+
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-
-        if (button.className === 'Number') {
-
-            if (equalButton === true) {
-                deleteNumberDisplayElements()
-                deleteOperationDisplayElements() 
-
-                VAL1 = null 
-                equalButton = false
-            }
-
+        if (button.className == 'Number') {
             if (numberDisplayController === true) {
-                deleteNumberDisplayElements()
+                deleteNumberDisplay()
                 numberDisplayController = false
             }
-
             numberDisplay[0].append(button.innerHTML)
             buttonStringContent += button.innerHTML
         }
-
-        if (button.className === 'Operator' && (buttonStringContent !== '' || equalButton === true)) {
-
+        
+        if (button.className == 'Operator' && buttonStringContent !== '') {
             numberDisplayController = true
+            operationDisplay[0].append(buttonStringContent)
 
-            if (equalButton === true) {
-                deleteOperationDisplayElements()
-                
-                buttonStringContent = VAL1
-                VAL2 = null
-                equalButton = false
-            }
+            if   (VAL1 === null) { VAL1 = parseInt(buttonStringContent) }
+            else                 { VAL2 = parseInt(buttonStringContent) }
 
-            operationDisplay[0].append(buttonStringContent) 
-            
-            if (VAL1 === null) { VAL1 = parseInt(buttonStringContent) }
-            else { VAL2 = parseInt(buttonStringContent)}
-            
-            if (operator === undefined) {
-                addArithmeticSymbol(button.id)
-                buttonStringContent = ''
-            }
-            
+            operationChooser(button.innerHTML)
+
             if (VAL1 !== null && VAL2 !== null) {
-                if (button.id === 'equal') {
-                    equalButton = true
-                }
-                calculator(VAL1, VAL2)
+                deleteNumberDisplay()
+                operationResult = calculator(VAL1, VAL2)
+                numberDisplay[0].append(operationResult)
+
+                VAL1 = operationResult
+                VAL2 = null
             }
         }
     })
 })
 
-function calculator(a, b) {
-
-    if (numberDisplay[0].hasChildNodes()) { deleteNumberDisplayElements() }
-
-    if (equalButton === true) {
-        operationDisplay[0].append('=')
+function calculator (a, b) {
+    if (typeOfOperation === '+') {
+        return a + b 
     }
-    else {
-        addArithmeticSymbol()
+    else if (typeOfOperation === '-') {
+        return a - b
     }
-
-    if (operator === 'sum') {
-        operationResult = a + b
+    else if (typeOfOperation === '*') {
+        return a * b
     }
-    else if (operator === 'subtract') {
-        operationResult = a - b
+    else if (typeOfOperation === '/') {
+        return a / b
     }
-    else if (operator === 'multiply') {
-        operationResult = a * b
-    }
-    else if (operator === 'divide') {
-        operationResult = a / b
-    }
-
-    numberDisplay[0].append(operationResult)
-    
-    VAL1 = operationResult
-    VAL2 = null
-    operator = undefined 
-    buttonStringContent = ''
-
 }
